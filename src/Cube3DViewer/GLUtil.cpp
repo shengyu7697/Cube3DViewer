7583a01da5912e90b1ba061aadf78d0d1ad66ced
shengyu <shengyu7697@gmail.com>
2018-03-28 09:57:09 +0800

    add cube color comment

--- src/Cube3DViewer/GLUtil.cpp
+++ src/Cube3DViewer/GLUtil.cpp
@@ -4,34 +4,58 @@
 
 namespace GLUtil {
 
+/* Cube Color Define
+ *           ┌ ─ ─ ─ ─ ┐
+ *           │ top     │
+ *           │ green   │
+ * ┌ ─ ─ ─ ─ ┼ ─ ─ ─ ─ ┼ ─ ─ ─ ─ ┬ ─ ─ ─ ─ ┐
+ * │ left    │ front   │ right   │ back    │
+ * │ blue    │ red     │ white   │ yellow  │
+ * └ ─ ─ ─ ─ ┼ ─ ─ ─ ─ ┼ ─ ─ ─ ─ ┴ ─ ─ ─ ─ ┘
+ *           │ bottom  │
+ *           │ orange  │
+ *           └ ─ ─ ─ ─ ┘
+ */
 void drawCube()
 {
     glBegin(GL_QUADS);
+
+    // top
     glColor3f(0.0, 1.0, 0.0); // green
     glVertex3f( 1.0, 1.0,-1.0);
     glVertex3f(-1.0, 1.0,-1.0);
     glVertex3f(-1.0, 1.0, 1.0);
     glVertex3f( 1.0, 1.0, 1.0);
-    glColor3f(1.0, 0.5, 0.0);
+
+    // bottom
+    glColor3f(1.0, 0.5, 0.0); // orange
     glVertex3f( 1.0,-1.0, 1.0);
     glVertex3f(-1.0,-1.0, 1.0);
     glVertex3f(-1.0,-1.0,-1.0);
     glVertex3f( 1.0,-1.0,-1.0);
+
+    // front
     glColor3f(1.0, 0.0, 0.0); // red
     glVertex3f( 1.0, 1.0, 1.0);
     glVertex3f(-1.0, 1.0, 1.0);
     glVertex3f(-1.0,-1.0, 1.0);
     glVertex3f( 1.0,-1.0, 1.0);
-    glColor3f(1.0, 1.0, 0.0);
+
+    // back
+    glColor3f(1.0, 1.0, 0.0); // yellow
     glVertex3f( 1.0,-1.0,-1.0);
     glVertex3f(-1.0,-1.0,-1.0);
     glVertex3f(-1.0, 1.0,-1.0);
     glVertex3f( 1.0, 1.0,-1.0);
+
+    // left
     glColor3f(0.0, 0.0, 1.0); // blue
     glVertex3f(-1.0, 1.0, 1.0);
     glVertex3f(-1.0, 1.0,-1.0);
     glVertex3f(-1.0,-1.0,-1.0);
     glVertex3f(-1.0,-1.0, 1.0);
+
+    // right
     glColor3f(1.0, 1.0, 1.0); // white
     glVertex3f( 1.0, 1.0,-1.0);
     glVertex3f( 1.0, 1.0, 1.0);
